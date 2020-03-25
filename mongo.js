bed61c674e24b285124509cfcc2025be8ecfd0e9
wzd <3051037702@qq.com>
2020-03-25 16:56:11 +0800

    修改上传

--- mongo.js
+++ mongo.js
@@ -1,4 +1,4 @@
-t MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 
 //const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
 const uri = "mongodb://192.168.37.190";
